84b116d8660be47fac6e8d666f2809408c048e3e
Hermes.alam <alam.kurniawan@mncgroup.com>
2021-05-24 07:02:46 +0700

    analisis

--- main.go
+++ main.go
@@ -30,15 +30,19 @@ func CronClean() {
 			if a != 0 {
 				fileName := config.DnsLog + filepath.Base(getFile)
 				var extension = filepath.Ext(fileName)
+				log.Println("MASUK 1");
 				if extension != ".filepart" {
+				log.Println("MASUK 2");
 					file, err := os.Open(fileName) // For read access.
 					partsFilename := strings.Split(file.Name(), "_")
 					partsDate := strings.Split(partsFilename[2], "-")
+					log.Println("MASUK 3");
 					//get date
 					date := partsDate[2] + "-" + partsDate[1] + "-" + partsDate[0]
 					if err != nil {
 						log.Println(err)
 					}
+					log.Println("MASUK 4");
 					scanner := bufio.NewScanner(file)
 					scanner.Split(bufio.ScanLines)
 					var txtlines []string
@@ -46,11 +50,13 @@ func CronClean() {
 					for scanner.Scan() {
 						txtlines = append(txtlines, scanner.Text())
 					}
+					log.Println("MASUK 5");
 
 					file.Close()
 					currentTime := time.Now()
 					dateNow := currentTime.Format("2006-01-02")
 					if date != dateNow {
+					log.Println("MASUK 6");
 						e := os.Remove(fileName)
 						if e != nil {
 							log.Println(e)
