0afdb0307cea469f7e96396a86e21930c19e2ad1
Hermes.alam <alam.kurniawan@mncgroup.com>
2021-06-01 19:38:13 +0700

    implemet clean all

--- main.go
+++ main.go
@@ -1,77 +1,92 @@
 package main
 
 import (
-	"bufio"
 	"cron-clean/config"
-	"log"
+	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
 
 	"github.com/jasonlvhit/gocron"
 )
 
 func main() {
 
-	gocron.Every(10).Seconds().Do(CronClean)
+	gocron.Every(1).Day().At("18:57").Do(CronClean)
 
 	<-gocron.Start()
 }
 
 func CronClean() {
-	var files []string
-	root := config.DnsLog
-	err := filepath.Walk(root, visit(&files))
+	err := ClearDir(config.DnsLog)
 	if err != nil {
-		log.Println(err)
-	} else {
-		for a, getFile := range files {
-			if a != 0 {
-				fileName := config.DnsLog + filepath.Base(getFile)
-				var extension = filepath.Ext(fileName)
-				if extension != ".filepart" {
-						file, err := os.Open(fileName) // For read access.
-						if err != nil {
-							partsFilename := strings.Split(file.Name(), "_")
-							partsDate := strings.Split(partsFilename[2], "-")
-							//get date
-							date := partsDate[2] + "-" + partsDate[1] + "-" + partsDate[0]
-							if err != nil {
-								log.Println(err)
-							}
-							scanner := bufio.NewScanner(file)
-							scanner.Split(bufio.ScanLines)
-							var txtlines []string
-
-							for scanner.Scan() {
-								txtlines = append(txtlines, scanner.Text())
-							}
-
-							file.Close()
-							currentTime := time.Now()
-							dateNow := currentTime.Format("2006-01-02")
-							if date != dateNow {
-								e := os.Remove(fileName)
-								if e != nil {
-									log.Println(e)
-								}
-								return
-							}
-							return
-						}
-				}
-			}
-		}
+		fmt.Println(err)
 	}
+	// var files []string
+	// root := config.DnsLog
+	// err := filepath.Walk(root, visit(&files))
+	// if err != nil {
+	// 	log.Println(err)
+	// } else {
+	// 	for a, getFile := range files {
+	// 		if a != 0 {
+	// 			fileName := config.DnsLog + filepath.Base(getFile)
+	// 			var extension = filepath.Ext(fileName)
+	// 			if extension != ".filepart" {
+	// 				file, err := os.Open(fileName) // For read access.
+	// 				if err != nil {
+	// 					partsFilename := strings.Split(file.Name(), "_")
+	// 					partsDate := strings.Split(partsFilename[2], "-")
+	// 					//get date
+	// 					date := partsDate[2] + "-" + partsDate[1] + "-" + partsDate[0]
+	// 					if err != nil {
+	// 						log.Println(err)
+	// 					}
+	// 					scanner := bufio.NewScanner(file)
+	// 					scanner.Split(bufio.ScanLines)
+	// 					var txtlines []string
+
+	// 					for scanner.Scan() {
+	// 						txtlines = append(txtlines, scanner.Text())
+	// 					}
+
+	// 					file.Close()
+	// 					currentTime := time.Now()
+	// 					dateNow := currentTime.Format("2006-01-02")
+	// 					if date != dateNow {
+	// 						e := os.Remove(fileName)
+	// 						if e != nil {
+	// 							log.Println(e)
+	// 						}
+	// 						return
+	// 					}
+	// 					return
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 }
 
-func visit(files *[]string) filepath.WalkFunc {
-	return func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Println(err)
-		}
-		*files = append(*files, path)
-		return nil
+// func visit(files *[]string) filepath.WalkFunc {
+// 	return func(path string, info os.FileInfo, err error) error {
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 		*files = append(*files, path)
+// 		return nil
+// 	}
+// }
+
+func ClearDir(dir string) error {
+	files, err := filepath.Glob(filepath.Join(dir, "*"))
+	if err != nil {
+		return err
 	}
+	for _, file := range files {
+		err = os.RemoveAll(file)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
